d40343d2a32ba7853006104e565da29b5d790297
flash-of-genius <amhdaar@gmail.com>
2022-06-29 15:29:42 +0100

    7 - Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,8 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBbQ_FkVK_wprKrY78MGG4IhUl3jzm4B7w";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: { part: "snippet", maxResults: 5, key: KEY },
+});
