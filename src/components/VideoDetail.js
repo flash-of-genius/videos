4d41e7c1001592656046d42a03bfe798a775b90c
flash-of-genius <amhdaar@gmail.com>
2022-07-01 15:47:12 +0100

    20 - Fixing a Few Warnings

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,7 +9,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSrc} frameborder="0"></iframe>
+        <iframe title="Video Player" src={videoSrc} frameborder="0"></iframe>
       </div>
       <div className="ui segment">
         <h4 className="ui heade">{video.snippet.title}</h4>
